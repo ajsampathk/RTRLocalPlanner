361b32a7069f4f19c6c2c506c3d8248654869d04
Abhijith Sampathkrishna <aj.sampathk@gmail.com>
2022-11-08 11:32:53 +0000

    Planning behaviour tested

--- rtr_local_planner/include/rtr_local_planner/rtr_local_planner.h
+++ rtr_local_planner/include/rtr_local_planner/rtr_local_planner.h
@@ -1,22 +1,57 @@
-#ifndef RTR_LOCAL_PLANNER_H_
-#define RTR_LOCAL_PLANNER_H_
+
+#ifndef RTR_LOCAL_PLANNER_ROS_H_
+#define RTR_LOCAL_PLANNER_ROS_H_
+
+#include <ros/ros.h>
 
 // abstract class from which our plugin inherits
 #include <nav_core/base_local_planner.h>
 
-#include <costmap_2d/costmap_2d_ros.h>
+// local planner specific classes which provide some macros
+#include <base_local_planner/goal_functions.h>
+
+// time
+#include <time.h>
+
+// msgs
 #include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
 #include <geometry_msgs/Twist.h>
-#include <ros/ros.h>
+#include <nav_msgs/Path.h>
+
+// tf2
+#include <tf2/utils.h>
 #include <tf2_ros/buffer.h>
 
-using namespace std;
+// costmap & geometry
+#include <costmap_2d/costmap_2d_ros.h>
+
+// other
+#include <array>
+#include <boost/bind.hpp>
+#include <boost/shared_ptr.hpp>
+#include <vector>
+
+// macros
+#define RAD(deg) 0.0174532925 * deg
+#define KP_LINEAR 0.75
+#define KP_ANGULAR 0.3
+#define INDEX_JUMP 5
+#define DISTANCE_THRESHOLD 0.1
+#define YAW_THRESHOLD_DEG 10
 
 namespace rtr_local_planner {
 
+struct pos {
+
+  double x, y, yaw, distance;
+};
+
 class RTRLocalPlanner : public nav_core::BaseLocalPlanner {
+
 public:
   RTRLocalPlanner();
+
   RTRLocalPlanner(std::string name, tf2_ros::Buffer *tf,
                   costmap_2d::Costmap2DROS *costmap_ros);
 
@@ -25,7 +60,7 @@ public:
   void initialize(std::string name, tf2_ros::Buffer *tf,
                   costmap_2d::Costmap2DROS *costmap_ros);
 
-  bool setPlan(const std::vector<geometry_msgs::PoseStamped> &orig_global_plan);
+  bool setPlan(const std::vector<geometry_msgs::PoseStamped> &global_plan);
 
   bool computeVelocityCommands(geometry_msgs::Twist &cmd_vel);
 
@@ -34,7 +69,39 @@ public:
 private:
   costmap_2d::Costmap2DROS *costmap_ros_;
   tf2_ros::Buffer *tf_;
+
+  // Topics & Services
+  ros::Subscriber amcl_sub;
+  ros::Publisher path_pub;
+
+  // Position Data
+  pos currentPos;
+  pos goalPos;
+  pos goalError;
+
+  // path plan data
+  int planLength;
+  int pathIndex;
+
+  // msg variables
+  std::vector<geometry_msgs::PoseStamped> plan;
+  geometry_msgs::Twist cmd;
+  geometry_msgs::PoseStamped goalPose;
+
+  // flags
+  bool amcl_started;
+  bool goal_reached_;
   bool initialized_;
+
+  void setVelocity(double linear, double angular);
+
+  void amclCallback(const geometry_msgs::PoseWithCovarianceStamped::Ptr &msg);
+
+  void updateError();
+
+  void updateGoal();
+
+  double directionalYaw(double yaw);
 };
 }; // namespace rtr_local_planner
 
--- rtr_local_planner/src/rtr_local_planner.cpp
+++ rtr_local_planner/src/rtr_local_planner.cpp
@@ -1,4 +1,6 @@
+
 #include "rtr_local_planner/rtr_local_planner.h"
+
 #include <pluginlib/class_list_macros.h>
 
 PLUGINLIB_EXPORT_CLASS(rtr_local_planner::RTRLocalPlanner,
@@ -17,39 +19,199 @@ RTRLocalPlanner::RTRLocalPlanner(std::string name, tf2_ros::Buffer *tf,
 
 RTRLocalPlanner::~RTRLocalPlanner() {}
 
-// Take note that tf::TransformListener* has been changed to tf2_ros::Buffer* in
-// ROS Noetic
 void RTRLocalPlanner::initialize(std::string name, tf2_ros::Buffer *tf,
                                  costmap_2d::Costmap2DROS *costmap_ros) {
+
   if (!initialized_) {
-    tf_ = tf;
+
     costmap_ros_ = costmap_ros;
+    tf_ = tf;
+
+    ros::NodeHandle gn;
+
+    amcl_sub =
+        gn.subscribe("amcl_pose", 100, &RTRLocalPlanner::amclCallback, this);
+    path_pub = gn.advertise<geometry_msgs::PoseStamped>("next_goal", 10);
+
+    amcl_started = 0;
+
     initialized_ = true;
+
+  } else {
+    ROS_WARN("Planner already initialized");
   }
 }
 
 bool RTRLocalPlanner::setPlan(
-    const std::vector<geometry_msgs::PoseStamped> &orig_global_plan) {
+    const std::vector<geometry_msgs::PoseStamped> &global_plan) {
+
   if (!initialized_) {
-    ROS_ERROR("This planner has not been initialized");
+    ROS_ERROR("Planner not initialized");
     return false;
   }
+
+  pathIndex = 1;
+
+  plan = global_plan;
+  planLength = (plan).size() - 1;
+  updateGoal();
+
+  ROS_INFO("Recieved Plan Size:%d", planLength);
+
+  goal_reached_ = false;
+
   return true;
 }
 
 bool RTRLocalPlanner::computeVelocityCommands(geometry_msgs::Twist &cmd_vel) {
+
   if (!initialized_) {
-    ROS_ERROR("This planner has not been initialized");
+    ROS_ERROR("Planner not inititalized");
     return false;
   }
+
+  if (planLength != 0) {
+
+    double final_yaw = 0;
+    double yaw_diff = 0;
+
+    updateError();
+
+    if (goalError.distance < DISTANCE_THRESHOLD) {
+
+      if (pathIndex < planLength) {
+
+        if ((planLength - pathIndex) < INDEX_JUMP) {
+          pathIndex = planLength;
+        } else {
+          pathIndex += INDEX_JUMP;
+        }
+        updateGoal();
+
+      } else {
+
+        ROS_INFO("Goal Reached");
+
+        final_yaw = tf2::getYaw(plan[planLength].pose.orientation);
+        yaw_diff = final_yaw - currentPos.yaw;
+
+        ROS_INFO("Final Orientation:%f", final_yaw);
+        ROS_INFO("Current Orientation:%f", currentPos.yaw);
+
+        yaw_diff = directionalYaw(yaw_diff);
+
+        ROS_INFO("Yaw Difference:%f", yaw_diff);
+
+        if (fabs(yaw_diff) > RAD(YAW_THRESHOLD_DEG)) {
+          setVelocity(0, yaw_diff * 0.5);
+        } else {
+          setVelocity(0, 0);
+          goal_reached_ = true;
+        }
+      }
+
+    } else {
+
+      if (fabs(goalError.yaw) > RAD(YAW_THRESHOLD_DEG)) {
+        ROS_INFO("Rotating-(YawError):%f", goalError.yaw);
+        setVelocity(0, goalError.yaw);
+
+      } else {
+        ROS_INFO("Translating-(Error):%f", goalError.distance);
+        setVelocity(goalError.distance, 0);
+      }
+    }
+    ROS_INFO("pathIndex:%d", pathIndex);
+  }
+
+  cmd_vel = cmd;
+
   return true;
 }
 
 bool RTRLocalPlanner::isGoalReached() {
   if (!initialized_) {
-    ROS_ERROR("This planner has not been initialized");
+    ROS_ERROR("Planner not initialized");
     return false;
   }
-  return false;
+  return goal_reached_;
 }
+
+void RTRLocalPlanner::amclCallback(
+    const geometry_msgs::PoseWithCovarianceStamped::Ptr &msg) {
+
+  pos previousPos;
+
+  if (amcl_started) {
+    previousPos.x = currentPos.x;
+    previousPos.y = currentPos.y;
+  }
+
+  currentPos.x = msg->pose.pose.position.x;
+  currentPos.y = msg->pose.pose.position.y;
+  currentPos.yaw = tf2::getYaw(msg->pose.pose.orientation);
+  updateError();
+
+  if (amcl_started) {
+    currentPos.distance += std::sqrt(
+        (currentPos.x - previousPos.x) * (currentPos.x - previousPos.x) +
+        (currentPos.y - previousPos.y) * (currentPos.y - previousPos.y));
+  }
+
+  amcl_started = 1;
+}
+
+void RTRLocalPlanner::setVelocity(double linear, double angular) {
+  cmd.linear.x = linear * KP_LINEAR;
+  cmd.linear.y = 0;
+  cmd.angular.z = angular * KP_ANGULAR;
+}
+
+void RTRLocalPlanner::updateGoal() {
+
+  goalPos.x = plan[pathIndex].pose.position.x;
+  goalPos.y = plan[pathIndex].pose.position.y;
+
+  goalPose.header.frame_id = "map";
+
+  goalPose.header.stamp = ros::Time::now();
+
+  goalPose.pose = plan[pathIndex].pose;
+
+  path_pub.publish(goalPose);
+}
+
+void RTRLocalPlanner::updateError() {
+
+  double yaw;
+
+  goalError.x = (goalPos.x - currentPos.x);
+  goalError.y = (goalPos.y - currentPos.y);
+
+  if (goalError.y == 0 & goalError.x == 0) {
+    yaw = currentPos.yaw;
+  } else {
+    yaw = std::atan2(goalError.y, goalError.x);
+  }
+
+  goalError.distance =
+      std::sqrt(goalError.x * goalError.x + goalError.y * goalError.y);
+  goalError.yaw = yaw - currentPos.yaw;
+
+  ROS_INFO("Distance to goal:%f", goalError.distance);
+  ROS_INFO("Yaw to Goal:%f", goalError.yaw);
+
+  goalError.yaw = directionalYaw(goalError.yaw);
+}
+
+double RTRLocalPlanner::directionalYaw(double yaw) {
+  if (yaw > RAD(180)) {
+    yaw -= RAD(360);
+  }
+  if (yaw < RAD(-180)) {
+    yaw += RAD(360);
+  }
+  return yaw;
+}
+
 } // namespace rtr_local_planner
