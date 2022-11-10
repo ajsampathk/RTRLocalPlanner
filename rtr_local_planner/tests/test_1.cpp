9beac787221f62a2cd4c7469644da1983f092547
Abhijith Sampathkrishna <aj.sampathk@gmail.com>
2022-11-10 15:53:10 +0000

    Added unit tests

--- rtr_local_planner/tests/test_1.cpp
+++ rtr_local_planner/tests/test_1.cpp
@@ -2,9 +2,6 @@
 
 #include <gtest/gtest.h>
 
-#include <chrono>
-#include <thread>
-
 using namespace rtr_local_planner;
 
 class BasicTestSuite : public ::testing::Test {
@@ -70,6 +67,40 @@ TEST_F(BasicTestSuite, yaw_ccw_change) {
   double yaw = rtr.directionalYaw(RAD(-225));
   double exp = RAD(360) - RAD(225);
   ASSERT_EQ(yaw, exp) << "Yaw should be +135 deg";
+}
+
+TEST_F(BasicTestSuite, pos_error_all) {
+  RTRLocalPLannerHelpers rtr;
+  pos target;
+  pos current;
+
+  target.x = 1;
+  target.y = 1;
+  current.x = 0;
+  current.y = 0;
+  pos error = rtr.getError(target, current);
+  ASSERT_EQ(error.x, 1) << "x error should be 1";
+  ASSERT_EQ(error.y, 1) << "x error should be 1";
+  ASSERT_DOUBLE_EQ(error.distance, std::sqrt(2))
+      << "distance should be sqrt(2)";
+  ASSERT_TRUE(fabs(error.yaw - RAD(45)) < 0.0001) << "Yaw should be 45deg";
+}
+
+TEST_F(BasicTestSuite, pos_error_yaw_singularity) {
+  RTRLocalPLannerHelpers rtr;
+  pos target;
+  pos current;
+
+  target.x = 0;
+  target.y = 0;
+  current.x = 0;
+  current.y = 0;
+  current.yaw = 0.174;
+  pos error = rtr.getError(target, current);
+  ASSERT_EQ(error.x, 0) << "x error should be 0";
+  ASSERT_EQ(error.y, 0) << "x error should be 0";
+  ASSERT_DOUBLE_EQ(error.distance, 0) << "distance should be sqrt(2)";
+  ASSERT_EQ(error.yaw, 0) << "Yaw should be 0";
 }
 
 int main(int argc, char **argv) {
